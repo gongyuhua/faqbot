49addfc1cc197f6fd110bc1def92a2f83e7fc041
gongyuhua <yg348@njit.edu>
2018-08-04 10:56:48 -0400

    Set queue driver to redis and use throttle for rate limiting.

--- app/Jobs/SendAnswerEmail.php
+++ app/Jobs/SendAnswerEmail.php
@@ -7,7 +7,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\QuestionAnswered;
 use App\Answer;
@@ -36,8 +36,14 @@ class SendAnswerEmail implements ShouldQueue
      */
     public function handle()
     {
+        // Allow only 2 emails every 1 second
+        Redis::throttle('my-mailtrap')->allow(2)->every(1)->then(function () {
         $recipient = 'steven@example.com';
         Mail::to($recipient)->send(new QuestionAnswered($this->Answer));
         Log::info('Emailed new answer ' . $this->Answer->id);
+        }, function () {
+            // Could not obtain lock; this job will be re-queued
+            return $this->release(2);
+        });
     }
 }
